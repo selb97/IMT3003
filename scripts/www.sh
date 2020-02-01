a1901764c86f699195776abda25c0f07659c8679
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-02-01 23:18:48 +0100

    Update www.sh

--- scripts/www.sh
+++ scripts/www.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y apache2 libapache2-mod-php php-pgsql
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y apache2 libapache2-mod-php php-pgsql
 sudo rm /var/www/html/index.html
 git clone  https://github.com/selb97/IMT3003.git
 cd IMT3003
