4b61935a97dc3730f79b322c2ac600bcf393b73e
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-02-02 17:01:26 +0100

    Create startContainers.sh

---
+++ scripts/startContainers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+sudo docker rm -f $(docker ps -qa)
+
+sudo docker build -t w1:6 /home/ubuntu/www1
+
+sudo docker run -itd -p 32770:80 --name "container1" --restart always www1:v6
+
+sudo docker run -itd -p 32772:80 --name "container2" --restart always www1:v6
+
+sudo docker run -itd -p 32774:80 --name "container3" --restart always www1:v6
+
+sudo docker run -itd -p 32776:80 --name "container4" --restart always www1:v6
