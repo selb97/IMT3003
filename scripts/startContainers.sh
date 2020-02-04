2e7f3a99eec451652692312aacf9eb6ef18209bd
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-02-04 16:12:45 +0100

    Update startContainers.sh

--- scripts/startContainers.sh
+++ scripts/startContainers.sh
@@ -4,16 +4,17 @@ sudo docker rm -f $(sudo docker ps -qa)
 
 sudo docker build -t WebContainer:v9 /home/ubuntu/www
 
-sudo docker run -itd -p 32771:80 --name "container1" --restart always WebContainer:v9
+sudo docker run -itd -p 32771:80 --name "container1" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32772:80 --name "container2" --restart always WebContainer:v9
+sudo docker run -itd -p 32772:80 --name "container2" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32773:80 --name "container3" --restart always WebContainer:v9
+sudo docker run -itd -p 32773:80 --name "container3" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32774:80 --name "container5" --restart always WebContainer:v9
+sudo docker run -itd -p 32774:80 --name "container5" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32775:80 --name "container6" --restart always WebContainer:v9
+sudo docker run -itd -p 32775:80 --name "container6" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32777:80 --name "container7" --restart always WebContainer:v9
+sudo docker run -itd -p 32777:80 --name "container7" --restart always webcontainer:v9
 
-sudo docker run -itd -p 32778:80 --name "container8" --restart always WebContainer:v9
+sudo docker run -itd -p 32778:80 --name "container8" --restart always webcontainer:v9
+
