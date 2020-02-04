acfc62173b448187df99eca3296de7ef41a73f78
Monastyr <60261397+Monastyr@users.noreply.github.com>
2020-02-04 13:33:42 +0100

    Update startContainers.sh

--- scripts/startContainers.sh
+++ scripts/startContainers.sh
@@ -2,12 +2,12 @@
 
 sudo docker rm -f $(sudo docker ps -qa)
 
-sudo docker build -t w1:6 /home/ubuntu/www1
+sudo docker build -t WebContainer:v9 /home/ubuntu/www1
 
-sudo docker run -itd -p 32770:80 --name "container1" --restart always www1:v6
+sudo docker run -itd -p 32770:80 --name "container1" --restart always WebContainer:v9
 
-sudo docker run -itd -p 32772:80 --name "container2" --restart always www1:v6
+sudo docker run -itd -p 32772:80 --name "container2" --restart always WebContainer:v9
 
-sudo docker run -itd -p 32774:80 --name "container3" --restart always www1:v6
+sudo docker run -itd -p 32774:80 --name "container3" --restart always WebContainer:v9
 
-sudo docker run -itd -p 32776:80 --name "container4" --restart always www1:v6
+sudo docker run -itd -p 32776:80 --name "container4" --restart always WebContainer:v9
