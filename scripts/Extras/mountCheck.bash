99a66977927e22ce64e6c18d199b57cec8a952a0
Monastyr <wojtaszek1910rts@gmail.com>
2020-04-10 11:44:59 +0000

    mountCheck

---
+++ scripts/Extras/mountCheck.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+df -h | grep bf
+
+
+RETVAL=$?
+IP=$(hostname -I)
+
+if [ $RETVAL -eq 0 ]
+then
+sudo service docker start
+else
+sudo mount -t glusterfs "$IP":bf_config /bf_config
+sudo mount -t glusterfs "$IP":bf_images /bf_images
+fi
